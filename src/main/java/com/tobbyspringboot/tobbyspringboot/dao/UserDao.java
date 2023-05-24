1f64b5641fa8c2b4dc8bc1fc5161008e8730d98e
trte135 <trte135@naver.com>
2023-05-24 14:27:58 +0900

    fix. tobby-spring[SimpleConnectionMaker, UserDao]  - Content :   - SimpleConnectionMaker   - UserDao

--- src/main/java/com/tobbyspringboot/tobbyspringboot/dao/UserDao.java
+++ src/main/java/com/tobbyspringboot/tobbyspringboot/dao/UserDao.java
@@ -3,32 +3,16 @@ package com.tobbyspringboot.tobbyspringboot.dao;
 import com.tobbyspringboot.tobbyspringboot.domain.User;
 
 import java.sql.*;
-import java.util.Map;
-
-import static java.lang.System.getenv;
 
 public class UserDao {
 		// Connection db method
-		public Connection getConnection() throws ClassNotFoundException, SQLException {
-				Map<String, String> env = getenv();
-				String dbHost = env.get("DB_HOST");
-				String dbUser = env.get("DB_USER");
-				String dbPassword = env.get("DB_PASSWORD");
-
-				Class.forName("com.mysql.cj.jdbc.Driver");
-
-				Connection conn = DriverManager.getConnection(
-								dbHost, dbUser, dbPassword
-				);
-				// conn 은 db를 연결해주는 값을 반환해줌
-				return conn;
-		}
+		SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
 
 		// Connection user data add method
 		public void add(User user) throws ClassNotFoundException, SQLException {
 				
 				// DB 연결
-				Connection conn = getConnection();
+				Connection conn = connectionMaker.makeNewConnection();
 				
 				PreparedStatement pstmt = conn.prepareStatement("insert into user(id, name, password) values(?, ?, ?)");
 				pstmt.setString(1, user.getId());
@@ -45,7 +29,7 @@ public class UserDao {
 		public User get(String id) throws ClassNotFoundException, SQLException {
 
 				// DB 연결
-				Connection conn = getConnection();
+				Connection conn = connectionMaker.makeNewConnection();
 
 				// db 와 연결하여 table 의 data 들을 불러오는 query 문을 작성
 				PreparedStatement pst = conn.prepareStatement("select id, name, password from user where id = ?");
@@ -76,13 +60,13 @@ public class UserDao {
 
 				User user = new User();
 
-				user.setId("2");
-				user.setName("test2");
+				user.setId("3");
+				user.setName("test3");
 				user.setPassword("1234");
 
 				userDao.add(user);
 
-				User selectedUser = userDao.get("2");
+				User selectedUser = userDao.get("3");
 
 				System.out.println(selectedUser.getId());
 				System.out.println(selectedUser.getName());
